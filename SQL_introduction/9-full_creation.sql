02e8ac319cce6d8b37bea111637f4f6748de9a42
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-18 11:18:45 +0100

    correct commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- create a table second_table in the database hbtn_0c_0
- CREATE TABLE IF NOT EXISTS second_table.hbtn_0c_0(
+ CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-
+-- Insert data into the second_table
 INSERT INTO second_table (id, name, score) 
 VALUES 
-    (1, 'John', 10);
-    (2, 'Alex', 3);
-    (3, 'Bob', 14);
-    (4, 'George', 8);
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8),
