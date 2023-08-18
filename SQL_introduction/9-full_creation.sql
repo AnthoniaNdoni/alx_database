36052c2337a35ac532a24fa91cf2ebda65f5b0a9
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-18 04:27:22 +0100

    Full Creation task commit

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- create a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXIST (hbtn_0c_0).second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO .hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
