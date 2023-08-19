1e086c1e012aacca7730081d820518504c4b8b73
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-19 22:01:21 +0100

    correct commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -5,8 +5,7 @@ CREATE TABLE hbtn_test_db_3.force_name (
     );
 
 --  Insert data into table
-INSERT INTO  hbtn_test_db_3.force_name (id, name) 
-VALUES
+INSERT INTO  hbtn_test_db_3.force_name (id, name) VALUES
     (1, 'Holberton School'),
     (2, 'Python is cool'),
     (3, 'Holberton'),
