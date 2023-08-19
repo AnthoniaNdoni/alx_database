99329ecf96c6f8f16a7c50d588ad3e929b0f1648
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-19 15:22:00 +0100

    correction commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- script that lists all privilages of the MYSQL usersuser_0d_1 and user_0d_2
 
-SHOW GRANTS FOR *.* TO 'user_0d_1 and user_0d_2 '@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1 '@'localhost', 'user_0d_2 '@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
