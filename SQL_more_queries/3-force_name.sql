ee31c8a2a23daacd18ec1d527b2d8857b5524e6e
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-19 23:33:07 +0100

    Always a name 3. commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,13 +1,5 @@
 -- script that create the table force_name on your MySQL server
-CREATE TABLE hbtn_test_db_3.force_name (
-    id INT PRIMARY KEY, 
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT, 
+    name VARCHAR(256) NOT NULL
     );
-
---  Insert data into table
-INSERT INTO  hbtn_test_db_3.force_name (id, name) VALUES
-    (1, 'Holberton School'),
-    (2, 'Python is cool'),
-    (3, 'Holberton'),
-    (4, 'School'),
-    (5, 'C is fun');
