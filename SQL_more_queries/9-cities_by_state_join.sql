35aa3de607e76fd87b655259d83f8a600bbcc66d
AnthoniaNdoni <tamarandoni@gmail.com>
2023-08-20 19:32:34 +0100

    INITIAL COMMIT for task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- script that lists  cities contained in the database hbtn_0d_usa
-SELECT cities.id , cities.name, states.name
-  FROM cities AS cities
-    INNER JOIN states AS state
-    ON cities_id = states.id
-  ORDER BY cities.id  
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id  ASC;
